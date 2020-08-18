d3bcd2aa87c6c0c0a99bc343b32d74b3a9ad60c8
Tom Weatherhead <tomw@2hrd4u.org>
2020-08-17 20:07:02 -0600

    0.0.1

---
+++ loader.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// Set options as a parameter, environment variable, or rc file.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require = require("esm")(module/*, options*/)
+
+// module.exports = require("./main.js")
+require('./lib/cli.js');
